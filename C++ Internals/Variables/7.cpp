42ddf410f4df35d3a87d3d1f02292472043b2991
Akanksha Gangrade <akankshagangrade27@gmail.com>
2026-08-26 09:39:35 +0530

    Studied copy semantics

---
+++ C++ Internals/Variables/7.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class strng
+{
+public:
+    char* str;
+    int size;
+    strng(const char *a)
+    {
+        cout<<"Constructor\n";
+        size=strlen(a);
+        str=new char(size+1);
+        memcpy(str, a, size);
+        str[size]=0;
+    }
+    strng(const strng& a)
+    {
+        cout<<"Copy constructor\n";
+        size=a.size;
+        str=new char(size+1);
+        memcpy(str, a.str, size);
+        str[size]=0;
+    }
+    ~strng()
+    {
+        cout<<"Destructor\n";
+        delete str;
+    }
+};
+int main()
+{
+    strng a("Hey there");
+    strng b=a;
+    return 0;
+}
