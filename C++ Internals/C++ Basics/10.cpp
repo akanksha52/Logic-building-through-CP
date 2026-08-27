151b1b923b505f2390ba8f7d704ce2307dfce911
Akanksha Gangrade <akankshagangrade27@gmail.com>
2026-08-27 11:14:02 +0530

    Studided virtual desctructor

--- C++ Internals/C++ Basics/10.cpp
+++ C++ Internals/C++ Basics/10.cpp
@@ -28,9 +28,41 @@ public:
 class Person2 : public Entity2
 {
 public:
-    string getName()
+    string getName() override
     {
         return "Person";
+    }
+};
+class Entity3
+{
+public:
+    ~Entity3()
+    {
+        cout<<"Entity3 destructor\n";
+    }
+};
+class Person3 : public Entity3
+{
+public:
+    ~Person3()
+    {
+        cout<<"Person3 destructor\n";
+    }
+};
+class Entity4
+{
+public:
+    virtual ~Entity4()
+    {
+        cout<<"Entity4 destructor\n";
+    }
+};
+class Person4 : public Entity4
+{
+public:
+    ~Person4() override
+    {
+        cout<<"Person4 destructor\n";
     }
 };
 int main()
@@ -53,5 +85,24 @@ int main()
     cout<<G->getName()<<endl;
     Entity2 *H = new Person2();
     cout<<H->getName()<<endl; 
+    cout<<"------------------------------------------\n";
+    {
+        Entity3 I;
+        Person3 J;
+    }
+    cout<<"------------------------------------------\n";
+    Entity3 *K = new Entity3();
+    Person3 *L = new Person3();
+    Entity3 *M = new Person3();
+    delete K;
+    delete L;
+    delete M;
+    cout<<"------------------------------------------\n";
+    Entity4 *N = new Entity4();
+    Person4 *O = new Person4();
+    Entity4 *P = new Person4();
+    delete N;
+    delete O;
+    delete P;
     return 0;
 }
