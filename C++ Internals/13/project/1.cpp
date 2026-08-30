d8c47a836df782d2f44cff640960db5c4ea976c9
Akanksha Gangrade <akankshagangrade27@gmail.com>
2026-08-30 13:34:44 +0530

    Studied the target_link_library() command

--- C++ Internals/13/project/1.cpp
+++ C++ Internals/13/project/1.cpp
@@ -1,9 +1,10 @@
 // CMake
 #include <iostream>
+#include <add.h>
 using namespace std;
 int main()
 {
     cout<<"Hello world\n";
-    // cout<<"Addition is: "<<add(2, 3)<<endl;
+    cout<<"Addition of 2 and 3 is: "<<add(2, 3)<<endl;
     return 0;
 }
