3b11460e744eb1bf182542a214b2a6e4e49a462e
Akanksha Gangrade <akankshagangrade27@gmail.com>
2026-08-30 23:35:33 +0530

    _

--- C++ Internals/12a.cpp
+++
@@ -1,8 +0,0 @@
-// Compilation
-#include <bits/stdc++.h>
-#include "12c.hpp"
-using namespace std;
-int add(int a, int b)
-{
-    return a + b;
-}
--- C++ Internals/3a.cpp
+++
@@ -1,3 +0,0 @@
-// Const 1
-#include "3c.h"
-extern const int x = 10;
